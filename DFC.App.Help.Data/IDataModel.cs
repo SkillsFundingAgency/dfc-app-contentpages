8a268cacf4fe89ff020de98150df9e897e4fa6fb
Ian Crisp <icrisp@program-systems.co.uk>
2019-07-31 09:34:28 +0100

    UNdone empty interface change

--- DFC.App.Help.Data/IDataModel.cs
+++ DFC.App.Help.Data/IDataModel.cs
@@ -1,8 +1,6 @@
 ﻿namespace DFC.App.Help.Data
 {
-#pragma warning disable CA1040 // Avoid empty interfaces
     public interface IDataModel
-#pragma warning restore CA1040 // Avoid empty interfaces
     {
     }
 }
