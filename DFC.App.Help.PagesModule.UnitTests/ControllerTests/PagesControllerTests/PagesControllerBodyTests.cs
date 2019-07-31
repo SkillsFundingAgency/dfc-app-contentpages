b22dc5545a30c6e98c119575add689042b3e6192
Ian Crisp <icrisp@program-systems.co.uk>
2019-07-31 13:29:09 +0100

    Added pages controller Body action unit tests

---
+++ DFC.App.Help.PagesModule.UnitTests/ControllerTests/PagesControllerTests/PagesControllerBodyTests.cs
@@ -0,0 +1,197 @@
+using DFC.App.Help.Data;
+using DFC.App.Help.ViewModels;
+using FakeItEasy;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using Xunit;
+
+namespace DFC.App.Help.PagesModule.UnitTests.ControllerTests.PagesControllerTests
+{
+    public class PagesControllerBodyTests : BasePagesController
+    {
+        [Theory]
+        [MemberData(nameof(HtmlMediaTypes))]
+        public async void PagesControllerBodyHtmlReturnsSuccess(string mediaTypeName)
+        {
+            // Arrange
+            const string article = "an-article-name";
+            var expectedResult = A.Fake<HelpPageModel>();
+            var controller = BuildPagesController(mediaTypeName);
+
+            A.CallTo(() => fakeHelpPageService.GetByNameAsync(A<string>.Ignored, A<bool>.Ignored)).Returns(expectedResult);
+            A.CallTo(() => fakeMapper.Map(A<HelpPageModel>.Ignored, A<BodyViewModel>.Ignored)).Returns(A.Fake<BodyViewModel>());
+
+            // Act
+            var result = await controller.Body(article).ConfigureAwait(false);
+
+            // Assert
+            A.CallTo(() => fakeHelpPageService.GetByNameAsync(A<string>.Ignored, A<bool>.Ignored)).MustHaveHappenedOnceExactly();
+            A.CallTo(() => fakeMapper.Map(A<HelpPageModel>.Ignored, A<BodyViewModel>.Ignored)).MustHaveHappenedOnceExactly();
+
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var model = Assert.IsAssignableFrom<BodyViewModel>(viewResult.ViewData.Model);
+
+            controller.Dispose();
+        }
+
+        [Theory]
+        [MemberData(nameof(JsonMediaTypes))]
+        public async void PagesControllerBodyJsonReturnsSuccess(string mediaTypeName)
+        {
+            // Arrange
+            const string article = "an-article-name";
+            var expectedResult = A.Fake<HelpPageModel>();
+            var controller = BuildPagesController(mediaTypeName);
+
+            A.CallTo(() => fakeHelpPageService.GetByNameAsync(A<string>.Ignored, A<bool>.Ignored)).Returns(expectedResult);
+            A.CallTo(() => fakeMapper.Map(A<HelpPageModel>.Ignored, A<BodyViewModel>.Ignored)).Returns(A.Fake<BodyViewModel>());
+
+            // Act
+            var result = await controller.Body(article).ConfigureAwait(false);
+
+            // Assert
+            A.CallTo(() => fakeHelpPageService.GetByNameAsync(A<string>.Ignored, A<bool>.Ignored)).MustHaveHappenedOnceExactly();
+            A.CallTo(() => fakeMapper.Map(A<HelpPageModel>.Ignored, A<BodyViewModel>.Ignored)).MustHaveHappenedOnceExactly();
+
+            var jsonResult = Assert.IsType<OkObjectResult>(result);
+            var model = Assert.IsAssignableFrom<BodyViewModel>(jsonResult.Value);
+
+            controller.Dispose();
+        }
+
+        [Theory]
+        [MemberData(nameof(JsonMediaTypes))]
+        public async void PagesControllerBodyJsonReturnsRedirectWhenAlternateArticleExists(string mediaTypeName)
+        {
+            // Arrange
+            const string article = "an-article-name";
+            HelpPageModel expectedResult = null;
+            var expectedAlternativeResult = A.Fake<HelpPageModel>();
+            var controller = BuildPagesController(mediaTypeName);
+
+            A.CallTo(() => fakeHelpPageService.GetByNameAsync(A<string>.Ignored, A<bool>.Ignored)).Returns(expectedResult);
+            A.CallTo(() => fakeHelpPageService.GetByAlternativeNameAsync(A<string>.Ignored)).Returns(expectedAlternativeResult);
+
+            // Act
+            var result = await controller.Body(article).ConfigureAwait(false);
+
+            // Assert
+            A.CallTo(() => fakeHelpPageService.GetByNameAsync(A<string>.Ignored, A<bool>.Ignored)).MustHaveHappenedOnceExactly();
+            A.CallTo(() => fakeHelpPageService.GetByAlternativeNameAsync(A<string>.Ignored)).MustHaveHappenedOnceExactly();
+
+            var statusResult = Assert.IsType<RedirectResult>(result);
+
+            statusResult.Url.Should().NotBeNullOrWhiteSpace();
+            A.Equals(true, statusResult.Permanent);
+
+            controller.Dispose();
+        }
+
+        [Theory]
+        [MemberData(nameof(HtmlMediaTypes))]
+        public async void PagesControllerBodyHtmlReturnsRedirectWhenAlternateArticleExists(string mediaTypeName)
+        {
+            // Arrange
+            const string article = "an-article-name";
+            HelpPageModel expectedResult = null;
+            var expectedAlternativeResult = A.Fake<HelpPageModel>();
+            var controller = BuildPagesController(mediaTypeName);
+
+            A.CallTo(() => fakeHelpPageService.GetByNameAsync(A<string>.Ignored, A<bool>.Ignored)).Returns(expectedResult);
+            A.CallTo(() => fakeHelpPageService.GetByAlternativeNameAsync(A<string>.Ignored)).Returns(expectedAlternativeResult);
+
+            // Act
+            var result = await controller.Body(article).ConfigureAwait(false);
+
+            // Assert
+            A.CallTo(() => fakeHelpPageService.GetByNameAsync(A<string>.Ignored, A<bool>.Ignored)).MustHaveHappenedOnceExactly();
+            A.CallTo(() => fakeHelpPageService.GetByAlternativeNameAsync(A<string>.Ignored)).MustHaveHappenedOnceExactly();
+
+            var statusResult = Assert.IsType<RedirectResult>(result);
+
+            statusResult.Url.Should().NotBeNullOrWhiteSpace();
+            A.Equals(true, statusResult.Permanent);
+
+            controller.Dispose();
+        }
+
+        [Theory]
+        [MemberData(nameof(HtmlMediaTypes))]
+        public async void PagesControllerBodyHtmlReturnsSuccessWhenNoAlternateArticle(string mediaTypeName)
+        {
+            // Arrange
+            const string article = "an-article-name";
+            HelpPageModel expectedResult = null;
+            HelpPageModel expectedAlternativeResult = null;
+            var controller = BuildPagesController(mediaTypeName);
+
+            A.CallTo(() => fakeHelpPageService.GetByNameAsync(A<string>.Ignored, A<bool>.Ignored)).Returns(expectedResult);
+            A.CallTo(() => fakeHelpPageService.GetByAlternativeNameAsync(A<string>.Ignored)).Returns(expectedAlternativeResult);
+
+            // Act
+            var result = await controller.Body(article).ConfigureAwait(false);
+
+            // Assert
+            A.CallTo(() => fakeHelpPageService.GetByNameAsync(A<string>.Ignored, A<bool>.Ignored)).MustHaveHappenedOnceExactly();
+            A.CallTo(() => fakeHelpPageService.GetByAlternativeNameAsync(A<string>.Ignored)).MustHaveHappenedOnceExactly();
+
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var model = Assert.IsAssignableFrom<BodyViewModel>(viewResult.ViewData.Model);
+
+            controller.Dispose();
+        }
+
+        [Theory]
+        [MemberData(nameof(JsonMediaTypes))]
+        public async void PagesControllerBodyJsonReturnsSuccessWhenNoAlternateArticle(string mediaTypeName)
+        {
+            // Arrange
+            const string article = "an-article-name";
+            HelpPageModel expectedResult = null;
+            HelpPageModel expectedAlternativeResult = null;
+            var controller = BuildPagesController(mediaTypeName);
+
+            A.CallTo(() => fakeHelpPageService.GetByNameAsync(A<string>.Ignored, A<bool>.Ignored)).Returns(expectedResult);
+            A.CallTo(() => fakeHelpPageService.GetByAlternativeNameAsync(A<string>.Ignored)).Returns(expectedAlternativeResult);
+
+            // Act
+            var result = await controller.Body(article).ConfigureAwait(false);
+
+            // Assert
+            A.CallTo(() => fakeHelpPageService.GetByNameAsync(A<string>.Ignored, A<bool>.Ignored)).MustHaveHappenedOnceExactly();
+            A.CallTo(() => fakeHelpPageService.GetByAlternativeNameAsync(A<string>.Ignored)).MustHaveHappenedOnceExactly();
+
+            var jsonResult = Assert.IsType<OkObjectResult>(result);
+            var model = Assert.IsAssignableFrom<BodyViewModel>(jsonResult.Value);
+
+            controller.Dispose();
+        }
+
+        [Theory]
+        [MemberData(nameof(InvalidMediaTypes))]
+        public async void PagesControllerBodyReturnsNotAcceptable(string mediaTypeName)
+        {
+            // Arrange
+            const string article = "an-article-name";
+            var expectedResult = A.Fake<HelpPageModel>();
+            var controller = BuildPagesController(mediaTypeName);
+
+            A.CallTo(() => fakeHelpPageService.GetByNameAsync(A<string>.Ignored, A<bool>.Ignored)).Returns(expectedResult);
+            A.CallTo(() => fakeMapper.Map(A<HelpPageModel>.Ignored, A<BodyViewModel>.Ignored)).Returns(A.Fake<BodyViewModel>());
+
+            // Act
+            var result = await controller.Body(article).ConfigureAwait(false);
+
+            // Assert
+            A.CallTo(() => fakeHelpPageService.GetByNameAsync(A<string>.Ignored, A<bool>.Ignored)).MustHaveHappenedOnceExactly();
+            A.CallTo(() => fakeMapper.Map(A<HelpPageModel>.Ignored, A<BodyViewModel>.Ignored)).MustHaveHappenedOnceExactly();
+
+            var statusResult = Assert.IsType<StatusCodeResult>(result);
+
+            A.Equals((int)HttpStatusCode.NotAcceptable, statusResult.StatusCode);
+
+            controller.Dispose();
+        }
+    }
+}
